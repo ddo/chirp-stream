959389ab6fc83da0c141794d662a38403a03ef83
Ddo <joeddo89@gmail.com>
2014-05-26 03:03:04 +0700

    add test file

---
+++ test.js
@@ -0,0 +1,28 @@
+var expect = require('chai').expect;
+
+var ChirpStream = require('./');
+
+describe("chirp-stream", function() {
+    describe("#stream", function() {
+        var twitter = new ChirpStream({
+            consumer: {
+                public: process.env.TWITTER_CONSUMER_PUBLIC,
+                secret: process.env.TWITTER_CONSUMER_SECRET
+            },
+            token: {
+                public: process.env.TWITTER_TOKEN_PUBLIC,
+                secret: process.env.TWITTER_SECRET_SECRET
+            }
+        });
+
+        it("should be return friend array", function(done) {
+            
+            var userstream = twitter.stream('https://userstream.twitter.com/1.1/user.json');
+
+            userstream.on('json', function(json) {
+                expect(json).to.have.property('friends').that.is.an('array');
+                done();
+            });
+        });
+    });
+});
