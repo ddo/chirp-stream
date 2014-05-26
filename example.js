64929d4b72dd067c1a3d7702551137e71df73ffe
Ddo <joeddo89@gmail.com>
2014-05-27 01:49:24 +0700

    update example

--- example.js
+++ example.js
@@ -2,18 +2,30 @@ var TwitterStream = require('./');
 
 var twitter = new TwitterStream({
     consumer: {
-        public: 'xxxx',
-        secret: 'xxxx'
+        public: process.env.TWITTER_CONSUMER_PUBLIC,
+        secret: process.env.TWITTER_CONSUMER_SECRET
     },
     token: {
-        public: 'xxxx',
-        secret: 'xxxx'
+        public: process.env.TWITTER_TOKEN_PUBLIC,
+        secret: process.env.TWITTER_SECRET_SECRET
     },
-    url: 'https://userstream.twitter.com/1.1/user.json'
+    url: 'https://userstream.twitter.com/1.1/user.jsonn'
 });
 
 twitter.on('data', function(data) {
+    console.log('data');
     console.log(data);
 });
+
+twitter.on('error', function(data) {
+    console.log('error');
+    console.log(data);
+});
+
+twitter.on('finish', function(data) {
+    console.log('finish');
+});
+
+
 
 twitter.stream();
